484be85fc9ead5fa72bfcc9b1adc67e7add9e022
Min Latt <minminlaxz@gmail.com>
2021-07-06 16:26:20 +0630

    Client socket

--- src/components/Chatroom/index.jsx
+++ src/components/Chatroom/index.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Chatbox from './Chatbox';
+import socketIOClient from "socket.io-client";
+
+const ENDPOINT = "http://localhost:3001"
+
 
 const Chatroom = () => {
-    console.log(io)
+
+    const [response, setResponse] = useState("");
+
+    useEffect(() => {
+        const socket = socketIOClient(ENDPOINT);
+        socket.on("FromAPI", data => {
+            setResponse(data);
+        });
+    }, []);
+
     return (
         <>
+            <p>
+                It's <time dateTime={response}>{response}</time>
+            </p>
             <Chatbox />
         </>
     );
