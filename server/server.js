a0dbc832aa75875ffbbd6425af7154a246508b58
Min Latt <minminlaxz@gmail.com>
2021-07-06 22:31:42 +0630

    clearInterval for server flooding

--- server/server.js
+++ server/server.js
@@ -1,12 +1,40 @@
 const express = require("express");
-const path = require("path");
+const http = require("http");
+const sockIo = require("socket.io");
+
 
 const app = express();
+const index = require("./routes/index");
+app.use(index);
 
-app.use(express.static(path.join(__dirname, "..", "build")))
+const httpServer = http.createServer(app);
 
-const PORT = process.env.PORT || 3001;
+const io = sockIo(httpServer, {
+    cors: {
+        origin : "http://localhost:3000",
+        methods : ["GET"]
+    }
+});
 
-app.listen(PORT, () => {
-    console.log(`Listening on port : ${PORT}`);
-})
+let interval;
+
+io.on("connection", (socket) => {
+  console.log(`New client connected: ID => ${socket.id}`);
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log(`Client disconnected: ID => ${socket.id}`);
+    clearInterval(interval);
+  });
+});
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+const port = process.env.PORT || 3002;
+httpServer.listen(port, () => console.log(`Listening on port ${port}`));
